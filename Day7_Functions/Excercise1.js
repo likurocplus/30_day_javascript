db3a1afb7da216be45589f64b7541d1d3dbe2e62
likurocplus <likuroquoc@gmail.com>
2023-12-16 07:44:30 +0700

    update

--- Day7_Functions/Excercise1.js
+++ Day7_Functions/Excercise1.js
@@ -54,18 +54,58 @@ let weight = function(mass,gravity) {
 weight(5,3)
 // Temperature in oC can be converted to oF using this formula: oF = (oC x 9/5) + 32. Write a function which convert oC to oF convertCelsiusToFahrenheit.
 
+let convertCelsiusToFahrenheit = (function(oC) {
+    console.log((oC * 2)*9.5 + 32) }
+)(5)
 // Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
-
 // The same groups apply to both men and women.
 // Underweight: BMI is less than 18.5
 // Normal weight: BMI is 18.5 to 24.9
 // Overweight: BMI is 25 to 29.9
 // Obese: BMI is 30 or more
+let BMI = function(height,weight){
+    let bmi = weight / ( height * height)
+    if(bmi < 18.5 ){
+        console.log(`Underweight`)
+    }
+    else if (bmi >= 18.5 && bmi <= 24.9){
+        console.log(`Normal weight`)
+    }
+    else if (bmi >=  30){
+        console.log(`Obese`)
+    } else {
+        console.log(`Over weight`)
+    }
+}
+BMI(52,1.8)
 // Write a function called checkSeason, it takes a month parameter and returns the season:Autumn, Winter, Spring or Summer.
-
+let month = new Date();
+let checkSeason = month => {
+    if(month >= 3 && month <=5){
+        console.log(`Spring`)
+    }
+    else if(month >=6 && month <= 8){
+        console.log(`Summer`)
+    }
+    else if(month >= 9 && month <=11){
+        console.log(`Autumn`)
+    }
+    else if(month>=12 && month <=2){
+        console.log(`Winter`)
+    }
+    else console.log(`default`)
+} 
+checkSeason(month.getMonth());
 // Math.max returns its largest argument. Write a function findMax that takes three arguments and returns their maximum with out using Math.max method.
-
 // console.log(findMax(0, 10, 5))
 // 10
 // console.log(findMax(0, -10, -2))
 // 0
+function findMax(a,b,c) {
+    let max = a
+    if(b>max) max = b
+    if(c>max) max = c
+    return max
+}
+console.log(findMax(0, 10, 5))
+console.log(findMax(0, -10, -2))
